beaa5862a87d04ca288d988b321158f201a5bec4
Fuzzychan <fuzzych4an@gmail.com>
2018-03-17 10:25:10 -0400

    my branch, they have my branch

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyATqoaVNAv5dim_qlP4AAgVe3CMGZLNF9Y",
+    authDomain: "binge-web-app.firebaseapp.com",
+    databaseURL: "https://binge-web-app.firebaseio.com",
+    projectId: "binge-web-app",
+    storageBucket: "",
+    messagingSenderId: "979328161042"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
