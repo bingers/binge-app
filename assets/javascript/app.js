9f749cf9cfb127aca6e4c7f9408bb2ae25aa3977
danielle frankel <daniellefrankel@danielle-frankels-MacBook-2.local>
2018-03-22 21:21:05 -0400

    finalsubmit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,9 +16,7 @@
   })
 
 
-
-
-  $("#submit").on("click", function (event) {
+$("#FinalSubmit").on("click", function (event){
       event.preventDefault();
       var foodSearch = $("#food-input").val().trim();
 
@@ -50,7 +48,43 @@
           $("#food-results").append(a, b, c, d);
       });
 
-  });
+});
+
+
+
+  // $("#submit").on("click", function (event) {
+  //     event.preventDefault();
+  //     var foodSearch = $("#food-input").val().trim();
+
+  //     // var foodSearch = "bacon";
+  //     var queryURL = "http://food2fork.com/api/search?key=96b3276309152fafb143690a0f191fa1&q=" + foodSearch + "&sort=r&count=5";
+  //     //var queryURL = "http://food2fork.com/api/search?key=96b3276309152fafb143690a0f191fa1&q=dessert&sort=r&count=5";
+
+  //     $.ajax({
+  //         url: queryURL,
+  //         method: "GET"
+  //     }).then(function (response) {
+  //         $("#food-results").empty();
+  //         var data = JSON.parse(response);
+  //         var recipeCount = data.count;
+  //         console.log("count: " + recipeCount);
+  //         var i = 0;
+
+  //         var a = $("<div>");
+  //         a.text(data.recipes[i].title);
+  //         var b = $("<div>");
+  //         var bImg = $("<img>");
+  //         bImg.attr("src", data.recipes[i].image_url);
+  //         b.append(bImg);
+  //         var c = $("<div>");
+  //         c.text(data.recipes[i].source_url);
+  //         var d = $("<div>");
+  //         d.text(data.recipes[i].social_rank);
+
+  //         $("#food-results").append(a, b, c, d);
+  //     });
+
+  // });
 
   // callback for successful getConfiguration call
   function configSuccessCallback(data) {
